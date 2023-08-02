a6f5e945bd08840ce49d34e3e1bf3dced9a9704e
rherik <herikrosa@hotmail.com>
2023-08-02 11:21:14 -0300

    Implements class MeuTwitter

---
+++ README.md
@@ -0,0 +1,18 @@
+<h1>Tarefas</h1>
+<ul>
+    <li>Refatorar método twita_and_delete</li>
+    <li>Descobrir uma api de imagens de memes</li>
+    <li>Limitador de enter ao excluir tweet</li>
+    <li>Documentar e comentar</li>
+    <li>Testar, testar, testar</li>
+    <li>Terminar configuração de def init</li>
+</ul>
+<h3>Testar:</h3>
+<ul>
+    <li>Verificar se a variável mensagem está vazia</li>
+</ul>
+<h3>Testado:</h3>
+<ul>
+    <li>Método twita</li>
+    <li>Método delete_tweet</li>
+</ul>
--- main.py
+++ main.py
@@ -3,80 +3,108 @@ import configparser
 import pandas as pd
 from time import sleep
 
-def api():
-    config = configparser.ConfigParser()
-    config.read('twikeys.ini')
-    auth = tweepy.OAuthHandler(config['twitter']['api_key'], config['twitter']['api_key_secret'])
-    auth.set_access_token(config['twitter']['access_token'], config['twitter']['access_token_secret'])
-    return tweepy.API(auth)
 
-def twita(api: tweepy.API, message: str, image_path=None):
-    try:
-        if image_path:
-            api.update_status_with_media(message, image_path)
+class MeuTwitter:
+    def __init__(self, api, user, mensagem):
+        self.api = api
+        self.user = user
+        if mensagem == '' or mensagem == ' ':
+            print("Tweet sem texto. Impossível executar.")
         else:
-            api.update_status(message)
-        print(f'Tweeted "{message}" successfully!')
-    except Exception as e:
-        print(f"Erro {e} ao postar tweet.")
-        
-def saves_user_tweets(api: tweepy.API, user, limit, termo=None):
-    tweets = tweepy.Cursor(api.user_timeline, screen_name=user, count=100, tweet_mode='extended').items(limit)
-    # tweets = api.user_timeline(screen_name=user, count=limit, tweet_mode='extended')
-    columns = [' Data ', '  Tweet ']
-    data = []
-    if termo != None:
-        for tweet in tweets:
-            if termo in tweet.full_text or termo.capitalize() in tweet.full_text:
+            self.message = mensagem
+
+    def twita(self, image_path=None):
+        try:
+            if image_path:
+                self.api.update_status_with_media(self.message, image_path)
+            else:
+                self.api.update_status(self.message)
+                print(f"~{self.message}~ twittado com sucesso!")
+        except Exception as e:
+            print(f"Erro {e} ao postar tweet.")
+
+    def delete_tweet(self):
+        try:
+            tweets = self.api.user_timeline()  # Obtém os últimos 20 tweets do usuário
+            print(len(tweets))
+            for tweet in tweets:
+                if tweet.text == self.message:
+                    tweet_id = tweet.id_str                
+                    self.api.destroy_status(tweet_id)
+                    print(f"Tweet deletado: {tweet.text}")
+                    break
+        except Exception as e:
+            print(f"Não foi possível deletar o tweet: ~{tweet.text}~ Erro: {e}")
+
+    def twita_and_delete(self):
+        user_tweets = tweepy.Cursor(
+            self.api.user_timeline, self.screen_name, count=100, tweet_mode='extended').items(10)            
+        for n in range(5):
+            sleep(1)
+            self.twita(api, f"{self.message} {n+1}º")
+
+        sleep(10)
+        for tweet in user_tweets:
+            if self.message in tweet.full_text:
+                certeza = input(
+                    f"O tweet ~{tweet.full_text}~ será excluído. Tem certeza que deseja excluí-lo?(s/n)")
+                sleep(1)
+                if certeza == n:
+                    print(f"{tweet.full_text} mantido na sua conta.")
+                else:
+                    self.delete_tweet(api, tweet_id=tweet.id,
+                                 tweet_text=tweet.full_text)
+                    print(f"{tweet.full_text} excluido.")
+
+    # USO DO ARQUIVO CSV:
+    def saves_user_tweets(self, limit, termo=None):
+        tweets = tweepy.Cursor(self.api.user_timeline, screen_name=self.user,
+                               count=100, tweet_mode='extended').items(limit)
+        # tweets = api.user_timeline(screen_name=user, count=limit, tweet_mode='extended')
+        columns = [' Data ', '  Tweet ']
+        data = []
+        if termo != None:
+            for tweet in tweets:
+                if termo in tweet.full_text or termo.capitalize() in tweet.full_text:
+                    data.append([tweet.created_at, tweet.full_text])
+                    # twita(api, f"@{user} tuitou sobre {termo}")
+        else:
+            for tweet in tweets:
                 data.append([tweet.created_at, tweet.full_text])
-                # twita(api, f"@{user} tuitou sobre {termo}")
+        df = pd.DataFrame(data, columns=columns)
+        return df
 
-    else:
+    def get_hastags(self, api: tweepy.API, keyword, limit):
+        tweets = tweepy.Cursor(api.search_tweets, q=keyword,
+                               count=100, tweet_mode='extended').items(limit)
+        columns = [' User ', '   Tweet ']
+        data = []
         for tweet in tweets:
-            data.append([tweet.created_at, tweet.full_text])
-    df = pd.DataFrame(data, columns=columns)
-    return df
-    
-def get_hastags(api: tweepy.API, keyword, limit):
-    tweets = tweepy.Cursor(api.search_tweets, q=keyword, count=100, tweet_mode='extended').items(limit)
-    columns = [' User ', '   Tweet ']
-    data = []
-    for tweet in tweets:
-        data.append([tweet.user.screen_name, tweet.full_text])
-    df = pd.DataFrame(data, columns=columns)
-    return df
+            data.append([tweet.user.screen_name, tweet.full_text])
+        df = pd.DataFrame(data, columns=columns)
+        return df
 
-def save_csv(dados):
-    dados.to_csv('tweets.csv')
-    
-def show_tweets():
-    with open('tweets.csv', 'r') as file:
-        for n in file:
-            print(n)
+    def save_csv(self, dados):
+    # save_csv(get_user_tweets(api, 'saobrisinha', 10))
 
-def delete_tweet(api, tweet_id, tweet_text):
-    try:
-        api.destroy_status(tweet_id)
-        print("Deleted tweet: ", tweet_text)
-    except:
-        print("Could not delete tweet: ", tweet_text)
+        dados.to_csv('tweets.csv')
+
+    def show_tweets_in_file(self):
+        with open('tweets.csv', 'r') as file:
+            for n in file:
+                print(n)
+
 
 if __name__ == '__main__':
-    texto_exemplo = ''
-    api = api()
-    # save_csv(get_user_tweets(api, 'saobrisinha', 10))
-    user_tweets = tweepy.Cursor(api.user_timeline, screen_name='saobrisinha', count=100, tweet_mode='extended').items(10)
-    for n in range(5):
-        sleep(1)
-        twita(api, f"{texto_exemplo} {n+1}º")
+    def api():
+        config = configparser.ConfigParser()
+        config.read('twikeys.ini')
+        auth = tweepy.OAuthHandler(
+            config['twitter']['api_key'], config['twitter']['api_key_secret'])
+        auth.set_access_token(
+            config['twitter']['access_token'], config['twitter']['access_token_secret'])
+        return tweepy.API(auth)
 
-    sleep(10)
-    for tweet in user_tweets:
-        #print(tweet.full_text)
-        if f"{texto_exemplo}" in tweet.full_text:
-            sleep(1)
-            delete_tweet(api, tweet_id=tweet.id, tweet_text=tweet.full_text)
-        # else:
-        #     print(f"Tweet {tweet.full_text}, de id{tweet.id} mantido.")
-    # for n in range(5):
-    #     twita(api, message=f'Tweet de teste {n+1}º')
+    api_herik = api()
+    herik = MeuTwitter(api_herik, "saobrisinha", "Testando classes")
+    herik.delete_tweet()
