4a3e11b22e6b4c02e86da25844749c7b12f1ffba
rherik <kurtzgk@gmail.com>
2023-08-27 15:37:13 -0300

    Adds new flask functions

--- run.py
+++ run.py
@@ -1,19 +1,23 @@
 import configparser
 from flask import Flask
-import requests
 
 def chave():
-    config = configparser.ConfigParser()
+    config = configparser.ConfigParser(interpolation=None)
     config.read('h_twikeys.ini')
     auth = config['twitter']['api_key'], config['twitter']['api_key_secret'], config['twitter']['bearer_token'], config['twitter']['access_token'], config['twitter']['access_token_secret']
     return auth
 
 app = Flask(__name__)
-app.config.from_object(chave())
-@app.route('/chaves', methods=['GET'])
+
+#app.config.from_object(chave())
+@app.get('/chaves')#, methods=['GET'])
 def root():
-    return 
+    chave = chave()
+    print(chave, type(chave))
+    return chave
 
 
 if __name__ == "__main__":
-    app.run()
+    #app.run()
+    chave = list(chave())
+    print(chave)
