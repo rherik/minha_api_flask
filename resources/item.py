369096ec758f1fc38a63fed4930233d731c098c8
rherik <herikrosa@hotmail.com>
2023-09-06 13:57:19 -0300

    begins marshmallow schemas validation

---
+++ resources/item.py
@@ -0,0 +1,50 @@
+import uuid
+from flask import request
+from flask.views import MethodView
+from flask_smorest import Blueprint, abort
+from db import items
+from schemas import ItemSchema, ItemUpdateSchema
+
+
+blp = Blueprint("items", __name__, description="Operations on items")
+
+@blp.route("/item/<string:item_id>")
+class item(MethodView):
+    def get(self, item_id):
+        try:
+            return items[item_id]
+        except KeyError:
+            abort(404, message="item not found.")
+
+    def delete(self, item_id):
+        try:
+            del items[item_id]
+            return {"message": "item deleted."}
+        except KeyError:
+            abort(404, message="item not found.")
+
+    @blp.arguments(ItemUpdateSchema)
+    def put(self, item_data, item_id):
+        try:
+            item = items[item_id]
+            item |= item_data
+            return item
+        except KeyError:
+            abort(404, message="Item not found.")
+
+
+@blp.route("/item")
+class itemList(MethodView):
+    def get(self):
+        return {"items": list(items.values())}
+    
+    @blp.arguments(ItemSchema)
+    def post(self, item_data):
+        for item in items.values():
+            if item_data["name"] == item["name"]:
+                abort(400, message="item already exists.")
+        
+        item_id = uuid.uuid4().hex
+        item = {**item_data, "id": item_id}
+        items[item_id] = item
+        return item
